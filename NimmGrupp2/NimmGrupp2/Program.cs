a75c50738bb49c0cf59984b68463cd16aeaf24af
Grumpylama <oscar.bjurman@gmail.com>
2021-10-18 19:30:29 +0200

    Added Files

---
+++ NimmGrupp2/NimmGrupp2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace NimmGrupp2
+{
+    class Program
+    {
+        //Main method
+        static void Main(string[] args)
+        {
+            bool x = true;
+            
+            
+            //Game loop  
+            while (x)
+            {
+
+                //x = false;
+            }
+            
+        }
+
+        void Draw()
+        {
+            
+            
+        }
+    }
+
+    public abstract class player
+    {
+        int score { get; set; }
+        
+
+    }
+
+    //Human class, for more players
+    public class Human : player
+    {
+        
+
+
+    }
+
+    public class EasyAI : player
+    {
+
+
+
+    }
+
+    public class GamerModeAI : player
+    {
+
+
+    }
+
+}
