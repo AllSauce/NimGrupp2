4d6b745fa9977c8e92a6526c74411ed99d731025
Grumpylama <65866382+Grumpylama@users.noreply.github.com>
2021-10-25 16:10:44 +0200

    Can now exit

--- GameLogic.cs
+++ GameLogic.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 
  namespace NimmGrupp2
- {
+{
     public class GameLogic
     {
         //Sets board:
@@ -16,7 +16,7 @@ using System.Collections.Generic;
         }
         // Removes sticks from board based on index and amount
         public void RemoveSticks(Tuple<int, int> input)
-        {
+            {
                 //Chosen stack 
                 int stack = input.Item1;
                 //Chosen amount of sticks to remove
@@ -24,16 +24,17 @@ using System.Collections.Generic;
 
                 //Replaces old value with new value
                 board[stack] = board[stack] - amount;
-        }
+            }
         // checks if game is over and adds score. Doesn't reset whose turn it is so that losing player starts next game
-        public void GameOver(bool player1Turn, Player player1, Player player2)
+         public void GameOver(bool player1Turn, Player player1, Player player2)
         {
             if(player1Turn && board[0] == 0 && board[1] == 0 && board[2] == 0)
             {
                 player1.score++;
                 //Draws Winning message
-                Drawer.DrawGameUI(board, player1Turn, player1, player2, player1.name +" Wins! Thanks for playing! Tap enter to play again!");
-                Console.ReadLine();
+                Drawer.DrawGameUI(board, player1Turn, player1, player2, player1.name +" Wins! Thanks for playing! Tap enter to play again! Type 'Quit' or 'Exit' to exit!");
+                string temp = Console.ReadLine();
+                if (temp == "exit" || temp == "quit" || temp == "Exit" || temp == "Quit"){Environment.Exit(0);}
                 //Resets board
                 board[0] = 5;
                 board[1] = 5;
@@ -45,7 +46,8 @@ using System.Collections.Generic;
                 player2.score++;
                 //Draws winning message
                 Drawer.DrawGameUI(board, player1Turn, player1, player2, player2.name +" Wins! Thanks for playing! Tap enter to play again!");
-                Console.ReadLine();
+                string temp = Console.ReadLine();
+                if (temp == "exit" || temp == "quit" || temp == "Exit" || temp == "Quit"){Environment.Exit(0);}
                 //Resets Board
                 board[0] = 5;
                 board[1] = 5;
